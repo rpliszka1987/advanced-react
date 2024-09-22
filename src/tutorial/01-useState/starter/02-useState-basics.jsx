a565e8575ce50bd1ad84a5fa906a301fc5ca274f
Robert Pliszka <robplisz@gmail.com>
2024-09-22 16:08:37 -0400

    Added a button to reset count in the useState example

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -6,10 +6,15 @@ const UseStateBasics = () => {
   const handleClick = () => {
     setCount(count + 1)
   }
+
+  const handleCountReset = () => {
+    setCount(0)
+  }
   return (
     <div>
       <h4>You clicked {count} times</h4>
       <button type="button" className="btn" onClick={handleClick}>Click me</button>
+      <button type="button" className="btn" onClick={handleCountReset}>Start over</button>
     </div>
   );
 };
