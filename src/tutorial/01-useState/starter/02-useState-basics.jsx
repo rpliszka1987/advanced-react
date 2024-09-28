5e0c05e1d60b6976af4db5930f6d66b4158f3e11
Robert Pliszka <robplisz@gmail.com>
2024-09-28 18:00:30 -0400

    Added add 10 button to increase count by 10

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -10,10 +10,15 @@ const UseStateBasics = () => {
   const handleCountReset = () => {
     setCount(0)
   }
+
+  const handleSkipCount = () => {
+    setCount(count + 10)
+  }
   return (
     <div>
       <h4>You clicked {count} times</h4>
       <button type="button" className="btn" onClick={handleClick}>Click me</button>
+      <button type="button" className="btn" onClick={handleSkipCount}>Add 10</button>
       <button type="button" className="btn" onClick={handleCountReset}>Start over</button>
     </div>
   );
