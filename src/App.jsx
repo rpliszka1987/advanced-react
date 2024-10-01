28f7b3718f936c09092abe396432084ccf4eceb7
Robert Pliszka <robplisz@gmail.com>
2024-09-30 21:17:57 -0400

    Setup useEffect to run depending on the state of each value

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Start from '../src/tutorial/02-useEffect/starter/02-useEffect-basics'
+import Start from '../src/tutorial/02-useEffect/starter/03-multiple-effects'
 
 function App() {
   return (
