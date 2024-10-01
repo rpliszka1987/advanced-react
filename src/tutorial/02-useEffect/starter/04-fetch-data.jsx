c067c5d8789bdc6a59f24e44051ec169ff19600d
Robert Pliszka <robplisz@gmail.com>
2024-09-30 21:50:29 -0400

    Added a fetch request to the fetch data from git to get users

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,26 @@
+import { useState, useEffect } from "react";
+
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
+  
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const fetchData = async() => {
+
+      try {
+        const response = await fetch(url);
+        const users = await response.json();
+        console.log(users)
+      } catch (error) {
+        console.log(error)
+      }
+      
+    } 
+    fetchData();
+  }, [])
+
   return <h2>fetch data example</h2>;
 };
 export default FetchData;
