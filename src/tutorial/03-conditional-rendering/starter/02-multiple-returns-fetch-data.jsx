071032ca8d152185fc7d008872567b045dafcab0
Robert Pliszka <robplisz@gmail.com>
2024-10-05 16:54:22 -0400

    Destructed the user data

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -36,16 +36,18 @@ const MultipleReturnsFetchData = () => {
     return <h2>There was an error</h2>;
   }
 
+  const {avatar_url, name, company, bio } = user;
+
   return (
     <div>
       <img
         style={{ width: '150px', borderRadius: '25px' }}
-        src={user.avatar_url}
-        alt={user.name}
+        src={avatar_url}
+        alt={name}
       />
-      <h2>{user.name}</h2>
-      <h4>Works at {user.company}</h4>
-      <p>{user.bio}</p>
+      <h2>{name}</h2>
+      <h4>Works at {company}</h4>
+      <p>{bio}</p>
     </div>
   );
 };
