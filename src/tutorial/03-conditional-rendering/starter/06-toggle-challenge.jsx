881dadc94f759006b7fce692984f50024f9ede0c
Robert Pliszka <robplisz@gmail.com>
2024-10-09 20:03:04 -0400

    Fixed the display and switching the display components

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -4,30 +4,27 @@ const ToggleChallenge = () => {
   const [isVisible, setIsVisible] = useState(false);
 
   const habndleButton = () => {
-    if(isVisible) {
-      setIsVisible(false)
-     
-    }
-    setIsVisible(true)
-    
-  }
+    isVisible ? setIsVisible(false) : setIsVisible(true);
+  };
 
   return (
     <div>
       <h1>Welcome</h1>
       {isVisible ? <Hello /> : <Goodbye />}
       {console.log(isVisible)}
-      <button className='btn' type='button' onClick={habndleButton}>{isVisible ? 'Hide' : 'Show'}</button>
+      <button className="btn" type="button" onClick={habndleButton}>
+        {isVisible ? 'Hide' : 'Show'}
+      </button>
     </div>
-  )
+  );
 };
 
 const Hello = () => {
-  return <h2>Hello</h2>
-}
+  return <h2>Hello</h2>;
+};
 
 const Goodbye = () => {
-  return <h2>Goodbye</h2>
-}
+  return <h2>Goodbye</h2>;
+};
 
 export default ToggleChallenge;
